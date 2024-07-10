9a5799c04795dcba041ad84db9b65bd7d936f612
TEEPUT <ciyi1594@gmail.com>
2024-07-10 19:36:51 +0900

    👍 CORS 성공!

---
+++ SpringCloudGateway/src/main/java/com/scg/config/RouteConfig.java
@@ -0,0 +1,29 @@
+package com.scg.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator ms1Route(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("ms1", r -> {
+                    return r.path("/ms1/**")
+                            .uri("http://localhost:9999");
+                })
+                .route("ms2", r -> {
+                    return r.path("/ms2/**")
+                            .uri("http://localhost:9998");
+                })
+                .route("ms3", r -> {
+                    return r.path("/ms3/**")
+                            .uri("http://localhost:9997");
+                })
+                .build();
+    }
+}
