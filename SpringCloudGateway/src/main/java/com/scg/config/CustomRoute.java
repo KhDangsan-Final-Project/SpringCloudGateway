707bdacc049060b1f83312c1e81bdf7c3513bc5e
TEEPUT <ciyi1594@gmail.com>
2024-07-10 14:07:41 +0900

    에러 수정

--- SpringCloudGateway/src/main/java/com/scg/config/CustomRoute.java
+++ SpringCloudGateway/src/main/java/com/scg/config/CustomRoute.java
@@ -8,6 +8,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CustomRoute {
 	
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    public CustomRoute(JwtAuthenticationFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+    
 	@Bean
     public RouteLocator ms1Route(RouteLocatorBuilder builder) {
 
@@ -15,6 +21,7 @@ public class CustomRoute {
                 .route("ms1", r -> r.path("/ms1/**")
                         .uri("http://localhost:9999"))
                 .route("ms2", r -> r.path("/ms2/**")
+                        .filters(f -> f.filter(jwtAuthFilter.apply(new JwtAuthenticationFilter.Config())))
                         .uri("http://localhost:9998"))
                 .route("ms3", r -> r.path("/ms3/**")
                         .uri("http://localhost:9997"))
