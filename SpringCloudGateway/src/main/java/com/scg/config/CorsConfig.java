caa3adb645faf5271401ab1bd320ef757ea0a484
TEEPUT <ciyi1594@gmail.com>
2024-07-18 17:37:36 +0900

    dev 의존성 추가 및 CORS 수정

--- SpringCloudGateway/src/main/java/com/scg/config/CorsConfig.java
+++ SpringCloudGateway/src/main/java/com/scg/config/CorsConfig.java
@@ -12,9 +12,8 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:8080");
-        corsConfiguration.addAllowedOrigin("http://localhost:3000");
-        corsConfiguration.addAllowedOrigin("http://finalfrontend.s3-website.ap-northeast-2.amazonaws.com");
+        corsConfiguration.addAllowedOriginPattern("*");
+//        corsConfiguration.addAllowedOrigin("http://finalfrontend.s3-website.ap-northeast-2.amazonaws.com");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setAllowCredentials(true);
